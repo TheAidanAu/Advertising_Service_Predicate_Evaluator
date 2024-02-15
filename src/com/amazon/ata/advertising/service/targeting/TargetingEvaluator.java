8a4445797f7b80f4b8350c0f78eac874c4012325
Aidan Au <aidan.au.linkedin@gmail.com>
2024-02-15 07:57:20 -0500

    S25 MT1 Milestone 1.1

--- src/com/amazon/ata/advertising/service/targeting/TargetingEvaluator.java
+++ src/com/amazon/ata/advertising/service/targeting/TargetingEvaluator.java
@@ -31,14 +31,17 @@ public class TargetingEvaluator {
     public TargetingPredicateResult evaluate(TargetingGroup targetingGroup) {
         List<TargetingPredicate> targetingPredicates = targetingGroup.getTargetingPredicates();
         boolean allTruePredicates = true;
-        for (TargetingPredicate predicate : targetingPredicates) {
-            TargetingPredicateResult predicateResult = predicate.evaluate(requestContext);
-            if (!predicateResult.isTrue()) {
-                allTruePredicates = false;
-                break;
-            }
-        }
 
+        allTruePredicates = targetingPredicates.stream()
+                .allMatch(predicate -> predicate.evaluate(requestContext).isTrue());
+//        // old code in a for-loop
+//        for (TargetingPredicate predicate : targetingPredicates) {
+//            TargetingPredicateResult predicateResult = predicate.evaluate(requestContext);
+//            if (!predicateResult.isTrue()) {
+//                allTruePredicates = false;
+//                break;
+//            }
+//        }
         return allTruePredicates ? TargetingPredicateResult.TRUE :
                                    TargetingPredicateResult.FALSE;
     }
